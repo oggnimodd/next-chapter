741d5d3e5426221542cf928ac60fcf2ed7895d99
oggnimodd <arnold.ds.dev@gmail.com>
2023-12-15 17:30:09 +0700

    [react] feat: add fab to baselayout

--- apps/next-chapter-react/src/layouts/BaseLayout/BaseLayout.tsx
+++ apps/next-chapter-react/src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,15 +1,27 @@
 import { FC } from "react";
 import { Header } from "components";
-import { Container } from "@mui/material";
+import { Container, Fab } from "@mui/material";
+import { useWindowScroll } from "@mantine/hooks";
+import { KeyboardArrowUp } from "@mui/icons-material";
 
 interface BaseLayoutProps {
   children: React.ReactNode;
 }
 
 const BaseLayout: FC<BaseLayoutProps> = ({ children }) => {
+  const [_scroll, scrollTo] = useWindowScroll();
+
   return (
     <>
       <Header />
+      <Fab
+        size="small"
+        color="primary"
+        className="z-50 fixed bottom-4 right-4"
+        onClick={() => scrollTo({ y: 0 })}
+      >
+        <KeyboardArrowUp />
+      </Fab>
       <Container className="w-full py-4 px-6 max-w-full">{children}</Container>
     </>
   );
