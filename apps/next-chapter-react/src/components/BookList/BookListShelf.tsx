4f242a79c6442a3b5ee30331702d3adb895c5f1c
oggnimodd <arnold.ds.dev@gmail.com>
2023-12-28 09:28:41 +0000

    [react] feat : add total book in shelf to BookListShelf

--- apps/next-chapter-react/src/components/BookList/BookListShelf.tsx
+++ apps/next-chapter-react/src/components/BookList/BookListShelf.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Book } from "@acme/db";
 import { Typography } from "@mui/material";
-import { CardShelf } from "components";
+import { CardShelf } from "components/Card";
 
 interface BookListShelfProps {
   books: Book[];
@@ -17,6 +17,10 @@ const BookListShelf: FC<BookListShelfProps> = ({ books, type }) => {
         color="primary"
       >
         {type}
+
+        <span className="text-white text-lg">{` (${books.length} ${
+          books.length === 1 ? "book" : "books"
+        })`}</span>
       </Typography>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
         {books.map((book) => (
