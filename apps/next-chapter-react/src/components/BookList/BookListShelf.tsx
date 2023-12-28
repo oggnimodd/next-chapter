6f5494553dbf47b52cc12bf16b8817ea9ca8ce42
oggnimodd <arnold.ds.dev@gmail.com>
2023-12-28 16:40:41 +0700

    [react] fix: BookList book count color

--- apps/next-chapter-react/src/components/BookList/BookListShelf.tsx
+++ apps/next-chapter-react/src/components/BookList/BookListShelf.tsx
@@ -18,9 +18,9 @@ const BookListShelf: FC<BookListShelfProps> = ({ books, type }) => {
       >
         {type}
 
-        <span className="text-white text-lg">{` (${books.length} ${
-          books.length === 1 ? "book" : "books"
-        })`}</span>
+        <span className="dark:text-white text-black/90 text-lg">{` (${
+          books.length
+        } ${books.length === 1 ? "book" : "books"})`}</span>
       </Typography>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
         {books.map((book) => (
