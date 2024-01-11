844fb2728a80c645680be4b20594a3ffab8a7c14
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-11 03:28:47 +0000

    [vue] feat : style NotFound page

--- apps/next-chapter-vue/src/pages/NotFound.vue
+++ apps/next-chapter-vue/src/pages/NotFound.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { CommonLayout } from "@/layouts";
+import { BaseLayout } from "@/layouts";
 </script>
 
 <template>
-  <CommonLayout :require-auth="false">
-    <h1>Not found</h1>
-    <router-link to="/">
-      <a>Back to home</a>
-    </router-link>
-  </CommonLayout>
+  <BaseLayout :require-auth="false">
+    <div class="flex mt-32 w-full flex-1 justify-center flex-col items-center gap-y-3">
+      <h1 class="font-bold">Page not found</h1>
+      <router-link to="/">
+        <VBtn color="primary">
+          <VIcon icon="mdi-home" class="mr-3" />
+          Back To Home
+        </VBtn>
+      </router-link>
+    </div>
+  </BaseLayout>
 </template>
