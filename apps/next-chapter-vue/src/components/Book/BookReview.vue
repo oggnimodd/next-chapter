3850fa91c49e3a02e327164ff92611c6b803af9b
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-16 05:02:18 +0000

    [vue] feat : book review components

--- apps/next-chapter-vue/src/components/Book/BookReview.vue
+++ apps/next-chapter-vue/src/components/Book/BookReview.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useBooksDetails } from "@/composables";
+import BookReviewCardAndForm from "./BookReviewCardAndForm.vue"
+
+const { id, isLoading, isError } = useBooksDetails();
+</script>
 
 <template>
-  <h1>Book Review</h1>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, quos.
-  </p>
+  <p v-if="isLoading">Loading...</p>
+  <p v-else-if="isError">Error...</p>
+  <div v-else-if="id" className="flex flex-col w-full lg:w-1/2">
+    <BookReviewCardAndForm :bookId="id" />
+  </div>
 </template>
