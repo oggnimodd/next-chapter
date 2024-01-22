90e20011eb2b8477de8292de7c5d2225fcb25ae1
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-22 02:20:44 +0000

    [react] fix: refetch shelves after generateInitialShelves

--- apps/next-chapter-react/src/hooks/useInitializeShelves.ts
+++ apps/next-chapter-react/src/hooks/useInitializeShelves.ts
@@ -2,8 +2,10 @@ import { useEffect } from "react";
 import { api } from "trpc";
 
 const useInitializeShelves = () => {
-  const { data: shelves, isLoading } = api.shelf.getAll.useQuery();
-  const { mutate: initialize } = api.shelf.generateInitialShelves.useMutation();
+  const { data: shelves, isLoading, refetch } = api.shelf.getAll.useQuery();
+  const { mutate: initialize } = api.shelf.generateInitialShelves.useMutation({
+    onSuccess: () => refetch(),
+  });
 
   useEffect(() => {
     if (!isLoading && shelves?.length === 0) {
