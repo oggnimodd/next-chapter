cf0a4b99634113d1daaa8e44776a35ce37d5e1fa
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-22 13:26:44 +0000

    [vue] feat: fix shelves order

--- apps/next-chapter-vue/src/pages/Home.vue
+++ apps/next-chapter-vue/src/pages/Home.vue
@@ -4,6 +4,7 @@ import { useMutation, useQuery } from "@tanstack/vue-query";
 import { BookListShelfPreview } from "@/components";
 import { BaseLayout } from "@/layouts";
 import { watch } from "vue"
+import { computed } from "vue";
 
 const { mutateAsync: initialize } = useMutation({
   mutationFn: api.shelf.generateInitialShelves.mutate,
@@ -30,6 +31,17 @@ watch(shelves, () => {
   }
 });
 
+const typeOrder = {
+  "Currently Reading": 0,
+  "To Be Read": 1,
+  Read: 2,
+};
+
+const sorted = computed(() => {
+  return shelves.value ? [...shelves.value].sort((a, b) => {
+    return typeOrder[a.type as keyof typeof typeOrder] - typeOrder[b.type as keyof typeof typeOrder]
+  }) : [];
+});
 </script>
 
 <template>
@@ -38,7 +50,7 @@ watch(shelves, () => {
       <p v-if="isLoading">Loading...</p>
       <p v-else-if="isError">Error...</p>
       <!-- The key needs to be like this, since vue reactivity systems would not detect the change in the shelf.book length when moving or removing a book -->
-      <BookListShelfPreview v-for="shelf in shelves" :key="shelf.id + shelf.Book.length" :title="shelf.type"
+      <BookListShelfPreview v-for="shelf in sorted" :key="shelf.id + shelf.Book.length" :title="shelf.type"
         :books="shelf.Book" :shelfId="shelf.id" />
     </div>
   </BaseLayout>
