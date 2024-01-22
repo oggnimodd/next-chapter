f948fea64c35e1c0085943a3ebfed5b67f28785f
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-22 04:00:47 +0000

    [react] feat: fix shelves order

--- apps/next-chapter-react/src/pages/Home.tsx
+++ apps/next-chapter-react/src/pages/Home.tsx
@@ -3,16 +3,28 @@ import { BaseLayout } from "layouts";
 import { api } from "trpc";
 import { BookListShelfPreview } from "components";
 
+const typeOrder = {
+  "Currently Reading": 0,
+  "To Be Read": 1,
+  Read: 2,
+};
+
 const Home: FC = () => {
   const { data: shelves, isLoading, isError } = api.shelf.getAll.useQuery();
 
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error...</p>;
 
+  const sorted = shelves.sort(
+    (a, b) =>
+      typeOrder[a.type as keyof typeof typeOrder] -
+      typeOrder[b.type as keyof typeof typeOrder],
+  );
+
   return (
     <BaseLayout>
       <div className="flex flex-col gap-4">
-        {shelves?.map((shelf) => (
+        {sorted?.map((shelf) => (
           <BookListShelfPreview
             key={shelf.id}
             title={shelf.type}
