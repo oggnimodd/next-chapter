b4ec528eba975f4893513e22fa63724efd1cdb99
oggnimodd <arnold.ds.dev@gmail.com>
2024-01-22 12:09:44 +0000

    [vue] fix: refetch shelves after generateInitialShelves

--- apps/next-chapter-vue/src/pages/Home.vue
+++ apps/next-chapter-vue/src/pages/Home.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
 import { api } from "@/trpc";
-import { useQuery } from "@tanstack/vue-query";
+import { useMutation, useQuery } from "@tanstack/vue-query";
 import { BookListShelfPreview } from "@/components";
 import { BaseLayout } from "@/layouts";
+import { watch } from "vue"
+
+const { mutateAsync: initialize } = useMutation({
+  mutationFn: api.shelf.generateInitialShelves.mutate,
+  onSuccess: () => {
+    refetch();
+  },
+});
 
 const {
   data: shelves,
+  refetch,
   isLoading,
   isError,
 } = useQuery({
   queryKey: ["shelves"],
   queryFn: () => api.shelf.getAll.query(),
 });
+
+
+// Watch for shelves, it there is no shelve than generate
+watch(shelves, () => {
+  if (shelves.value?.length === 0) {
+    initialize();
+  }
+});
+
 </script>
 
 <template>
@@ -20,13 +38,8 @@ const {
       <p v-if="isLoading">Loading...</p>
       <p v-else-if="isError">Error...</p>
       <!-- The key needs to be like this, since vue reactivity systems would not detect the change in the shelf.book length when moving or removing a book -->
-      <BookListShelfPreview
-        v-for="shelf in shelves"
-        :key="shelf.id + shelf.Book.length"
-        :title="shelf.type"
-        :books="shelf.Book"
-        :shelfId="shelf.id"
-      />
+      <BookListShelfPreview v-for="shelf in shelves" :key="shelf.id + shelf.Book.length" :title="shelf.type"
+        :books="shelf.Book" :shelfId="shelf.id" />
     </div>
   </BaseLayout>
 </template>
